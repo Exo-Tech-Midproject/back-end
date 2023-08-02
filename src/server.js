27b6f025c8225d0c5f5da69b74049b8142f7e652
abdullah-aljoulani <abdullahaljoulani@gmail.com>
2023-08-02 14:51:09 +0000

    Hasan & Abdullah worked on it

--- src/server.js
+++ src/server.js
@@ -3,20 +3,26 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// require('dotenv').config();
 
+const errorHandler = require('./error-handlers/500');
+const notFound = require('./error-handlers/404');
 // const morgan = require('morgan');
 const router = express.Router();
 
-router.param
+// router.param
 
-
-
-
+const physicianRouter = require('./routes/physicianRoutes/physicianRoutes');
 
 app.use(express.json())
 app.use(cors())
 // app.use(morgan())
+
+// Routes
+app.use(physicianRouter)
+
+// Catchalls
+app.use(notFound);
+app.use(errorHandler);
 
 function start(port){
     app.listen(port , ()=> console.log(`up and running on port ${port}`))
