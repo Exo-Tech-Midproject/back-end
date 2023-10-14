c28949d7184b232bb12fe116ab9bde2d9d2a7c4c
Haznto <haznto@gmail.com>
2023-10-14 07:51:42 +0000

    removed FT

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 3001;
 const { db } = require('./src/models')
 const { start } = require('./src/server')
 
-db.sync({force:true}).then(() => {
+db.sync().then(() => {
     start(PORT);
 })
